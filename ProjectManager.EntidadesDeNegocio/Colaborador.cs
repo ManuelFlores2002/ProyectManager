b556cc09bdb9ba30fbaa49a19bc2d6a4897a8edc
Manuel Flores <manuelvillalta819@gmail.com>
2023-09-07 17:21:58 -0600

    Codificacion de la Clase Entidades de Colaborador

--- ProjectManager.EntidadesDeNegocio/Colaborador.cs
+++ ProjectManager.EntidadesDeNegocio/Colaborador.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectManager.EntidadesDeNegocio
 {
-    internal class Colaborador
+    public class Colaborador
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Proyecto")]
+        [ForeignKey("Proyecto")]
+        public int IdProyecto { get; set; }
+        public Proyecto Proyecto { get; set; }
+
+        [Display(Name ="Usuario")]
+        [ForeignKey("Usuario")]
+        [Required(ErrorMessage ="El ID de Usuario es obligatorio")]
+        public int IdUsuario { get; set; }
+        public Usuario Usuario { get; set;}
+
+        [Display(Name ="Estado")]
+        [Required(ErrorMessage ="El Estado del colaborador es obligatorio")]
+        public byte Estado { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estado_Colaborador
+    {
+        ACTIVO = 1,
+        INACTIVO = 0
     }
 }
